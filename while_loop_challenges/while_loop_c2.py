47aa9301eb29fd0b024ce4da3b85dfdd21f81d4e
MShafqatS <mohammad.siddiqui@gemsessence.com>
2023-07-12 20:05:22 +0530

    while_llop is added

---
+++ while_loop_challenges/while_loop_c2.py
@@ -0,0 +1,14 @@
+# while_loop_challenge2
+# printing only even numbers in while loop
+
+# Initialize Value
+i=0
+
+# Loop Condition
+while i<10:
+  # check for even number
+  if i % 2 == 0:
+    print(i, end=" ")
+
+  # Updating Value
+  i += 1
