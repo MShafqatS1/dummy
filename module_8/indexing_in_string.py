51a843d24537d69d1df2f5307e4005abd07221aa
MShafqatS <mohammad.siddiqui@gemsessence.com>
2023-07-13 19:45:03 +0530

    module 8 created

---
+++ module_8/indexing_in_string.py
@@ -0,0 +1,7 @@
+# Indexing in a String
+
+name = "Mohammad Shafqat Siddiqui"
+
+print(len(name))
+print(name[0])
+print(name[-3])
---
+++ module_8/string_slicing.py
@@ -0,0 +1,10 @@
+# String Slicing
+# I don't want Moh from name
+
+name = "Mohammad Shafqat Siddiqui"
+
+print(len(name))
+print(name[0])
+
+# print initail value then after every 5 characters it will print
+print(name[::5])
